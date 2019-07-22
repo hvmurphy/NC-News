37bb6fc89b4499a4ad70727113d9442be758fe61
Holly <holly@Hollys-MacBook-Air.local>
2019-07-22 17:03:35 +0100

    adds open github link in new page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className="footer">
       Visit my{" "}
-      <a href="https://github.com/hvmurphy" className="github">
+      <a href="https://github.com/hvmurphy" className="github" target="_blank">
         GitHub <img src={icon} alt="GitHub Logo" />
       </a>
     </div>
